caa988e4e757305087377f174985aa1432e99a71
Ibrahim Isa <ibrahimd8@yahoo.com>
2017-03-29 11:43:37 +0100

    New token

--- index.js
+++ index.js
@@ -20,30 +20,25 @@ app.get("/", function(req, res) {
 let token = "EAAaMy6zQ2hMBAMuyDhdy4wxmEZCYSNNutgk0lbiOfyGmDXdJZBEAi0e7i4f1EiCuZAkZAnUNx7NHFfdotFyOv8YULMJ7YZBVSUwR8cl25YXzF4ILXxXpM5tV8l192ZCnZA5SoUBZBOkLKZAwfoZCPZBfZCeWw9ArM0qPUF6dFD7p6XE8KwZDZD"
 
 // Facebook
-app.get("/webhook/", function(req, res) {
-	if (req.query["hub.verify_token"] === "cardsbotNg") {
-		res.send(req.query["hub.challenge"])
-		//Get Started Page
-		getStarted()
+app.get('/webhook/',function(req, res){
+	if(req.query['hub.verify_token']=="cardsbotNg"){
+		res.send(req.query['hub.challenge'])
 	}
-	res.send("Wrong token")
+	res.send("wrong token")
 })
 
-app.post("/webhook/", function (req,res) {
-
-	//Persistent Menu
-	PersistentMenu()
-
+app.post('/webhook/',function(req,res){
 	let messaging_events = req.body.entry[0].messaging
-	for (let i = 0; i < messaging_events.length; i++) {
+	for (let i = 0; i < messaging_events.length; i++){
 		let event = messaging_events[i]
 		let sender = event.sender.id
-		if (event.message && event.message.text) {
+		if(event.message && event.message.text)
+		{
 			let text = event.message.text
 			decideMessage(sender, text)
 		}
 
-		if (event.postback) {
+		if(event.postback){
 			let text = JSON.stringify(event.postback)
 			decideMessage(sender, text)
 			continue
@@ -52,164 +47,124 @@ app.post("/webhook/", function (req,res) {
 	res.sendStatus(200)
 })
 
-function decideMessage(sender, text1) {
+function decideMessage(sender, text1)
+{
 	let text = text1.toLowerCase()
-	if (text.includes("summer")) {
+	if(text.includes("summer")){
 		sendImageMessage(sender)
-	} else if (text.includes("winter")) {
+	}else if(text.includes("winter")){
 		sendGenericMessage(sender)
-	} else {
+	}else{
 		sendText(sender, "I like Fall")
-		sendButtonMessage(sender, "What is your favorite season?")
-		// send question
+		sendButtonMessage(sender, "what is your favourite season")
 	}
 }
-
-
-function sendText(sender, text) {
+/**
+ * Description : Send Text Message
+ */
+function sendText(sender, text)
+{
 	let messageData = {text: text}
 	sendRequest(sender, messageData)
 }
-
-function sendButtonMessage(sender, text) {
+/**
+ * Description : Send Button Message
+ */
+function sendButtonMessage(sender, text)
+{
 	let messageData = {
-		"attachment":{
-      		"type":"template",
-      		"payload":{
-		        "template_type":"button",
-		        "text": text,
-		        "buttons":[
-		          {
-		            "type":"postback",
-		            "title":"Summer",
-		            "payload":"summer"
-		          },
-		          {
-		            "type":"postback",
-		            "title":"Winter",
-		            "payload":"winter"
-		          }
-		        ]
-      		}
-    	}
+		    "attachment":{
+      						"type":"template",
+      						"payload":{
+        								"template_type":"button",
+        								"text": text,
+								        "buttons":[
+								          {
+								            "type"    : "postback",
+								            "title"   : "Summer",
+								            "payload" : "summer"
+								          },
+								          {
+								            "type"    : "postback",
+								            "title"   : "Winter",
+								            "payload" : "winter"
+								          }]
+      								  }
+    					}
 	}
 	sendRequest(sender, messageData)
 }
-
-function sendImageMessage(sender) {
-	let messageData = {
-    "attachment":{
-      "type":"image",
-      "payload":{
-        "url":"https://chipsifraternity.files.wordpress.com/2012/05/summer-picture1.jpg"
-      }
-    }
-  }
-  sendRequest(sender, messageData)
-}
-
-function sendGenericMessage(sender) {
-	let messageData = {
-		"attachment":{
-	      "type":"template",
-	      "payload":{
-	        "template_type":"generic",
-	        "elements":[
-	           {
-	            "title":"Winter!",
-	            "image_url":"http://static2.visitfinland.com/wp-content/uploads/Header_Kaskinen_winter.jpg",
-	            "subtitle":"I love Winter!",
-	            "buttons":[
-	              {
-	                "type":"web_url",
-	                "url":"https://en.wikipedia.org/wiki/Winter",
-	                "title":"More about winter"
-	              },          
-	            ]      
-	          }
-	        ]
-	      }
-    	}
-	}
-	sendRequest(sender, messageData)
-}
-
-function sendRequest(sender, messageData) {
-	request({
-		url: "https://graph.facebook.com/v2.6/me/messages",
-		qs : {access_token : token},
-		method: "POST",
-		json: {
-			recipient: {id: sender},
-			message: messageData
-		}
-
-	}, function(error, response, body) {
-		if (error) {
+/**
+ * Description : Send Image Message
+ */
+ function sendImageMessage(sender)
+ {
+ 	let messageData = {
+	    				"attachment":{
+	      								"type":"image",
+	      								"payload":{	
+	      											"url":"http://cdn.hercampus.com/s3fs-public/2015/06/22/Summer_2015.jpg"
+	                                              }
+	    				             }
+ 	}
+ 	sendRequest(sender, messageData)
+ }
+/**
+ * Description : Send Generic Message
+ */
+ function sendGenericMessage(sender){
+ 	let messageData = {
+					 	  "attachment":{
+					      "type":"template",
+					      "payload":{
+					        "template_type":"generic",
+					        "elements":[
+					           {
+					            "title":"Winter",
+					            "image_url":"http://advancedtreecare.ca/images/winter_treecare_newmarket_ontario.png",
+					            "subtitle":"I love winter",
+					            "default_action": {
+					              "type": "web_url",
+					              "url": "https://peterssendreceiveapp.ngrok.io/view?item=103",
+					              "messenger_extensions": true,
+					              "webview_height_ratio": "tall",
+					              "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+					            },
+					            "buttons":[
+					              {
+					                "type":"web_url",
+					                "url":"https://en.wikipedia.org/wiki/Winter",
+					                "title":"More about winter"
+					              }              
+					            ]      
+					          }
+					        ]
+					      }
+					    }
+ 	}
+ 	sendRequest(sender, messageData)
+ }
+/**
+ * Description : Send Request
+ */
+ function sendRequest(sender, messageData){
+    request({
+		url    : "https://graph.facebook.com/v2.6/me/messages",
+		qs     : {access_token : token},
+		method : "POST",
+		json   : {
+				   recipient : {id: sender},
+				   message   : messageData
+		         }
+	}, function(error, response, body){
+		if(error) {
 			console.log("sending error")
-		} else if (response.body.error) {
+		}else if(response.body.error){
 			console.log("response body error")
 		}
 	})
+ }
 
-}
-
-function PersistentMenu(){
- request({
-    url: 'https://graph.facebook.com/v2.6/me/thread_settings',
-    qs: { access_token: token },
-    method: 'POST',
-    json:{
-        setting_type : "call_to_actions",
-        thread_state : "existing_thread",
-        call_to_actions:[
-            {
-              type:"postback",
-              title:"Search Cards",
-              payload:"search cards"
-            },
-            {
-              type:"postback",
-              title:"Create Card",
-              payload:"create card"
-            },
-            {
-              type:"postback",
-              title:"Talk to a human",
-              payload:"talk to a human"
-            }
-          ]
-    }
-
-}, function(error, response, body) {
-    console.log(response)
-    if (error) {
-        console.log('Error sending messages: ', error)
-    } else if (response.body.error) {
-        console.log('Error: ', response.body.error)
-    }
-})
-
-}
-
-function getStarted() {
-	request({
-		url: 'https://graph.facebook.com/v2.6/me/thread_settings',
-    	qs: { access_token: token },
-    	method: 'POST',
-    	json: {
-    		"setting_type":"call_to_actions",
-			"thread_state":"new_thread",
-			"call_to_actions":[
- 				{
- 					"payload":"get started"
- 				}
-			]
-    	}
-	})
-}
-
-
-app.listen(app.get("port"), function() {
+app.listen(app.get('port'), function(){
 	console.log("running: port")
 })
