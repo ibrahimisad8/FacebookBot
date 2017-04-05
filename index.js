7058c11f141c9870e4b24c3638d1b6972d27ea39
Ibrahim Isa <ibrahimd8@yahoo.com>
2017-04-05 11:20:03 +0100

    Page access token

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ function processPostback(event) {
   var payload = event.postback.payload;
   payload = payload.toLowerCase();
 
-  if (payload === "Greeting") {
+  if (payload === "greeting") {
     greeting(senderId);
   }
 }
