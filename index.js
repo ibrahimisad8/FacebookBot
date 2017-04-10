2dda1d07ad2aee1c431b5d4fcc35c1e3b3209b38
Ibrahim Isa <ibrahimd8@yahoo.com>
2017-04-10 15:02:48 +0100

    models-mosvies

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const bodyParser = require("body-parser");
 // Set mongoose 
 var mongoose = require("mongoose");
 var db    = mongoose.connect(process.env.MONGODB_URI);
-var Movie = require("./models/movie");
+var Movie = require("./models/movies");
 // Intialize app use 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
