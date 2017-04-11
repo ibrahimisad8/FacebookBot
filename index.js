d2dfd1a3911941a3aa9f84088f46fad5a0716cae
Ibrahim Isa <ibrahimd8@yahoo.com>
2017-04-11 15:47:28 +0100

    Greetings

--- index.js
+++ index.js
@@ -66,6 +66,7 @@ function processPostback(event) {
     var payload = event.postback.payload;
 
     if (payload === "Greeting") {
+        greeting(senderId);
         // Get user's first name from the User Profile API
         // and include it in the greeting
         request({
@@ -210,4 +211,116 @@ function sendMessage(recipientId, message) {
             console.log("Error sending message: " + response.error);
         }
     });
+}
+/**
+ * Description : Processes message Sequences
+ */
+function greeting(senderId){
+  // Get user's first name from the User Profile API
+  // and include it in the greeting
+  request({
+      url: "https://graph.facebook.com/v2.6/" + senderId,
+      qs: {
+        access_token: PAGE_ACCESS_TOKEN,
+        fields: "first_name"
+      },
+      method: "GET"
+    }, function(error, response, body) {
+      var greeting = "";
+      if (error) {
+        console.log("Error getting user's name: " +  error);
+      } else {
+        var bodyObj = JSON.parse(body);
+        var name    = bodyObj.first_name;
+        greeting    = "Hi " + name + ". ";
+      }
+      var message   = greeting;
+
+      sendMessage(senderId, {text: message});              
+
+      setTimeout(function() {
+          sendMessage(senderId, {text: "Welcome to Cards."});
+      }, 1000)
+
+      setTimeout(function() {
+          greetingReplies(senderId);
+      }, 2000)
+    });
+}
+/**
+ * Description : Processes Mesage replies 
+ */
+function greetingReplies(senderId){
+  let message = {
+    "text":"What would you like to do?",
+    "quick_replies":[
+      {
+        "content_type":"text",
+        "title":"Search Cards",
+        "payload":"search"
+      },
+      {
+        "content_type":"text",
+        "title":"Create Card",
+        "payload":"create"
+      },
+      {
+        "content_type":"text",
+        "title":"Talk to a human",
+        "payload":"human"
+      }
+    ]
+  }
+  sendMessage(senderId, message);
+}
+/**
+ * Description : Processe Cards searching
+ */
+function searchCards(senderId) {
+  let message = {
+    "text":"Pick a category below or type in exactly what you are looking for.",
+    "quick_replies":[
+      {
+        "content_type":"text",
+        "title":"food",
+        "payload":"food"
+      },
+      {
+        "content_type":"text",
+        "title":"fashion",
+        "payload":"fashion"
+      },
+      {
+        "content_type":"text",
+        "title":"shoes",
+        "payload":"shoes"
+      },
+      {
+        "content_type":"text",
+        "title":"travel",
+        "payload":"travel"
+      },
+      {
+        "content_type":"text",
+        "title":"hospitality",
+        "payload":"hospitality"
+      },
+      {
+        "content_type":"text",
+        "title":"entertainment",
+        "payload":"entertainment"
+      },
+      {
+        "content_type":"text",
+        "title":"electronics",
+        "payload":"electronics"
+      },
+      {
+        "content_type":"text",
+        "title":"gifts",
+        "payload":"gifts"
+      }
+    ]
+  }
+  sendMessage(senderId, message);
 }
