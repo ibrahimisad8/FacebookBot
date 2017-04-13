a9bd87695ea6cb324af954d4a665b42b45470c3b
Ibrahim Isa <ibrahimd8@yahoo.com>
2017-04-13 16:31:57 +0100

    Corsel Testing

--- index.js
+++ index.js
@@ -26,6 +26,27 @@ app.listen((process.env.PORT || 5000));
 app.get("/", function (req, res) {
     res.send("Deployed!");
 });
+/*/ Var Data - Base
+var cardsData = [
+                  {"Business Name":"Hib Soft Solutions","name":"Ibrahim Isa","email":"ibrahimd8@yahoo.com","web":"www.iblynks.com","phone":"07068869376","Address":"D6 Garbja Abdulkadir Road Ungwan Rimi","Service":"Engineer","ImageUrl":"https://d125fmws0bore1.cloudfront.net/assets/udacity_share-46db4b8faf075a5af5a1070a7fa0ad3639783609ff45f447e4ea467fe3aa9d32.png"},
+                  
+                  {"Business Name":"Sadiq Travels","name":"Sadiq Adinoyi","email":"sadiq@yahoo.com","web":"www.sadiq.com","phone":"07068869376","Address":"D6 Garbja Abdulkadir Road Ungwan Rimi","Service":"travel","ImageUrl":"http://travelfashiongirl.com/wp-content/uploads/2016/12/best-travel-shoes-cover.jpg"},
+                  
+                  {"Business Name":"David shoes & Accesories","name":"David John","email":"david@yahoo.com","web":"www.david.com","phone":"07068869376","Address":"D6 Garbja Abdulkadir Road Ungwan Rimi","Service":"shoes","ImageUrl":"https://static.pexels.com/photos/19090/pexels-photo.jpg"},
+                  
+                  {"Business Name":"","name":"Robbart Monoma","email":"robbart@yahoo.com","web":"www.robbart.com","phone":"07068869376","Address":"D6 Garbja Abdulkadir Road Ungwan Rimi","Service":"designer","ImageUrl":"https://i.ytimg.com/vi/6nTKbT8JybQ/maxresdefault.jpg"},
+                  
+                  {"Business Name":"","name":"Zainab","email":"zainab@yahoo.com","web":"www.zainab.com","phone":"07068869376","Address":"D6 Garbja Abdulkadir Road Ungwan Rimi","Service":"travel","ImageUrl":"http://crisscrosstvl.com/wp-content/uploads/2016/05/sunset-plane.png"},
+                  
+                  {"Business Name":"","name":"Abba Sulieman","email":"abba@yahoo.com","web":"www.abba.com","phone":"07068869376","Address":"D6 Garbja Abdulkadir Road Ungwan Rimi","Service":"Food","ImageUrl":"https://images7.alphacoders.com/368/368875.jpg"},
+                  
+                  {"Business Name":"","name":"Sanusi Ismaila","email":"sanusi@yahoo.com","web":"www.sanusi.com","phone":"07068869376","Address":"D6 Garbja Abdulkadir Road Ungwan Rimi","Service":"shoes","ImageUrl":"https://s-media-cache-ak0.pinimg.com/originals/e8/6c/ef/e86cef21233114bb6a7fa665462cd56d.jpg"},
+                  
+                  {"Business Name":"","name":"Ifeanyi Moh","email":"ifeanyi@yahoo.com","web":"www.ifeanyi.com","phone":"07068869376","Address":"D6 Garbja Abdulkadir Road Ungwan Rimi","Service":"travel","ImageUrl":"https://2xd7m81yfswf20bam1231iux-wpengine.netdna-ssl.com/wp-content/uploads/2014/08/Santorini_iStock_000039729572_Full-1-2000x1250.jpg"},
+                  
+                  {"Business Name":"","name":"Musa Kenedy","email":"musa@yahoo.com","web":"www.musa.com","phone":"07068869376","Address":"D6 Garbja Abdulkadir Road Ungwan Rimi","Service":"Food","ImageUrl":"https://s3.amazonaws.com/htw/dt-contest-entries/73034/germany-restaurant-beer-food-beverage-bar-wine-Beef-logo-design.png"},
+
+                ];*/
 // Facebook Webhook
 // Used for verification
 app.get("/webhook", function (req, res) {
@@ -117,9 +138,14 @@ function processMessage(event) {
             {
                 searchCards(senderId)
             }
+            else if(checkMsg.includes("food")||checkMsg.includes("shoes")||checkMsg.includes("travel"))
+            {
+              cardsData(senderId);
+            }
             else
             {
-                sendMessage(senderId, {text: "Sorry, I don't understand your request "+checkMsg});
+                sendMessage(senderId, {text: "Sorry, I don't understand your request -> "+checkMsg+".Please pick one"});
+                searchCards(senderId);
             }
 
         } else if (message.attachments) {
@@ -127,7 +153,6 @@ function processMessage(event) {
         }
     }
 }
-
 // sends message to user
 function sendMessage(recipientId, message) {
     request({
@@ -185,11 +210,6 @@ function searchCards(senderId) {
       },
       {
         "content_type":"text",
-        "title":"fashion",
-        "payload":"fashion"
-      },
-      {
-        "content_type":"text",
         "title":"shoes",
         "payload":"shoes"
       },
@@ -197,28 +217,76 @@ function searchCards(senderId) {
         "content_type":"text",
         "title":"travel",
         "payload":"travel"
-      },
-      {
-        "content_type":"text",
-        "title":"hospitality",
-        "payload":"hospitality"
-      },
-      {
-        "content_type":"text",
-        "title":"entertainment",
-        "payload":"entertainment"
-      },
-      {
-        "content_type":"text",
-        "title":"electronics",
-        "payload":"electronics"
-      },
-      {
-        "content_type":"text",
-        "title":"gifts",
-        "payload":"gifts"
       }
     ]
   }
   sendMessage(senderId, message);
 }
+/**
+ * Description : CardsData
+ */
+ function CardsData(senderId)
+ {
+    let message = {"message":{
+                              "attachment":{
+                                              "type":"template",
+                                              "payload":{ "template_type":"generic",
+                                                          "elements":[
+                                                                        {
+                                                                            "title":"Welcome to Peter\'s Shoes",
+                                                                            "image_url":"https://static.pexels.com/photos/19090/pexels-photo.jpg",
+                                                                            "subtitle" :"We\'ve got the right hat for everyone.",
+                                                                            "default_action": {
+                                                                            "type": "web_url",
+                                                                            "url" : "https://peterssendreceiveapp.ngrok.io/view?item=103",
+                                                                            "messenger_extensions": true,
+                                                                            "webview_height_ratio": "tall",
+                                                                            "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                                                                                             },
+                                                                            "buttons":[
+                                                                                        {
+                                                                                          "type":"web_url",
+                                                                                          "url":"https://petersfancybrownhats.com",
+                                                                                          "title":"View Website"
+                                                                                        }             
+                                                                                      ]      
+                                                                        },
+                                                                        {
+                                                                            "title":"Hib Soft Solutions",
+                                                                            "image_url":"https://d125fmws0bore1.cloudfront.net/assets/udacity_share-46db4b8faf075a5af5a1070a7fa0ad3639783609ff45f447e4ea467fe3aa9d32.png",
+                                                                            "subtitle" :"Software & Hardware Engineer. 34 Slack Road , Tel:07068869376",
+                                                                            "default_action": {
+                                                                            "type": "web_url",
+                                                                            "url" : "https://peterssendreceiveapp.ngrok.io/view?item=103",
+                                                                            "messenger_extensions": true,
+                                                                            "webview_height_ratio": "tall",
+                                                                            "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                                                                                             },
+                                                                            "buttons":[
+                                                                                        {
+                                                                                          "type":"web_url",
+                                                                                          "url":"https://iblynks.com",
+                                                                                          "title":"View Website"
+                                                                                        }             
+                                                                                      ]      
+                                                                        },
+                                                                        {
+                                                                            "title":"Sadiq Travels",
+                                                                            "image_url":"http://travelfashiongirl.com/wp-content/uploads/2016/12/best-travel-shoes-cover.jpg",
+                                                                            "subtitle" :"Professional Travel Agents. D5 Washinton DC, Tel:+1284740373",
+                                                                            "buttons":[
+                                                                                        {
+                                                                                          "type":"web_url",
+                                                                                          "url":"https://iblynks.com",
+                                                                                          "title":"View Website"
+                                                                                        }             
+                                                                                      ]      
+                                                                        }
+                                                                      ]
+                                                         }                                               
+                                              }
+                            } 
+                    }
+    // Send Message
+    sendMessage(senderId, message);
+ }
